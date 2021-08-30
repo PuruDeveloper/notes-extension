54510f123b730f4d4c718338f0b8f7a5ec64aa0b
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2021-08-30 19:59:07 +0530

    New version. Users can now add manual inputs and save tabs and delete all notes with one click.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-It is a notes tracker extension to write notes when on the browser.
+# It is a notes tracker extension to write notes when on the browser.
+
+### users can add notes and save them with SAVE INPUT button.
+
+### users can save tabs with one click on SAVE TAB button.
+
+### users can delete all the notes with one click on DELETE ALL button.
--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 let myNotes = [];
+
 const inputEl = document.getElementById("input-el");
 
 const inputBtn = document.getElementById("input-btn");
 
 const ulEl = document.getElementById("ul-el");
 
+const deleteBtn = document.getElementById("delete-btn");
+
+const tabBtn = document.getElementById("tab-btn");
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs);
+    myNotes.push(tabs[0].url);
+    localStorage.setItem("myNotes", JSON.stringify(myNotes));
+    render(myNotes);
+  });
+});
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myNotes"));
+if (leadsFromLocalStorage) {
+  myNotes = leadsFromLocalStorage;
+  render(myNotes);
+}
+
 inputBtn.addEventListener("click", function () {
   if (inputEl.value) {
     myNotes.push(inputEl.value);
     inputEl.value = "";
-    renderNotes();
+    localStorage.setItem("myNotes", JSON.stringify(myNotes));
+    render(myNotes);
   }
 });
 
-function renderNotes() {
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myNotes = [];
+  render(myNotes);
+});
+
+function render(notes) {
   let listItems = "";
-  for (let i = 0; i < myNotes.length; i++) {
+  for (let i = 0; i < notes.length; i++) {
     listItems += `
     <li>
-        ${myNotes[i]}
+        ${notes[i]}
+        <button>Delete</button>
     </li>`;
-    // const li = document.createElement("li");
-    // li.textContent = myNotes[i];
-    // ulEl.append(li);
   }
   ulEl.innerHTML = listItems;
 }
+
+// function deleteIndividual(index) {
+//   myNotes.filter()
+// }
