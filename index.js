579c26d18fb8e2963e4a8a16d4d918f108d409c9
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2021-09-01 19:19:01 +0530

    Now the users can easily delete individual notes if need be.

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 
 tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log(tabs);
-    myNotes.push(tabs[0].url);
-    localStorage.setItem("myNotes", JSON.stringify(myNotes));
-    render(myNotes);
-  });
+  console.log(location.href);
+  myNotes.push(location.href);
+  localStorage.setItem("myNotes", JSON.stringify(myNotes));
+  render(myNotes);
 });
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myNotes"));
@@ -46,12 +44,18 @@ function render(notes) {
     listItems += `
     <li>
         ${notes[i]}
-        <button>Delete</button>
+        <button id="delete-indi" onclick="deleteIndividual(${i})">Delete</button>
     </li>`;
   }
   ulEl.innerHTML = listItems;
 }
 
-// function deleteIndividual(index) {
-//   myNotes.filter()
-// }
+/*deleteIndividual(i) takes index of the element need to be deleted and filters the array and removes it from the array and updates the localStorage with new array*/
+function deleteIndividual(i) {
+  console.log(i);
+  myNotes = myNotes.filter((note, index) => {
+    return index != i;
+  });
+  localStorage.setItem("myNotes", JSON.stringify(myNotes));
+  render(myNotes);
+}
